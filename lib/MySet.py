a2284b41eb860e4889fc656f45dfadec85c86ad6
Abednego <abedikroos15@gmail.com>
2023-06-10 23:34:37 +0300

    MySet class

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,24 @@
 class MySet:
 
-    pass
+    def __init__(self, initial_set=None):
+        self.dictionary = {}
+        if initial_set is not None:
+            for item in initial_set:
+                self.dictionary[item] = True
+
+    def add(self, item):
+        self.dictionary[item] = True
+
+    def delete(self, item):
+        if item in self.dictionary:
+            del self.dictionary[item]
+
+    def has(self, item):
+        return item in self.dictionary
+
+    def size(self):
+        return len(self.dictionary)
+
+
+
+    
